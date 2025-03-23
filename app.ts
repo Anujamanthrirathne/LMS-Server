fab97fc81bf6036d586286426a8b15b794313dba
anuja <anujamanthriratne@gmail.com>
2025-03-23 05:27:10 -0700

    res

--- app.ts
+++ app.ts
@@ -55,21 +55,21 @@ app.use(cors({
 
 
 
-const corsMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-  res.setHeader("Access-Control-Allow-Origin", "https://e-learning-client-two.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+// const corsMiddleware = (req: Request, res: Response, next: NextFunction): void => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://e-learning-client-two.vercel.app");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-};
+//   if (req.method === "OPTIONS") {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// };
 
-// Use the middleware separately
-app.use(corsMiddleware);
+// // Use the middleware separately
+// app.use(corsMiddleware);
 
 // api request limit
 const limiter = rateLimit({
